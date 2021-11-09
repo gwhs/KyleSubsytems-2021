db01527df0edc6c89ec8e00378044116cdd1c2a5
Chris <chvoong@s.sfusd.edu>
2021-11-08 19:40:42 -0800

    added imported libraries

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,21 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +26,15 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    private final XboxController m_driverController = new XboxController(0);
+    private static final int m_myRobotLeftEnd = 6;
+    private static final int m_myRobotLeftMiddle = 5;
+    private static final int m_myRobotLeftFront = 4;
+    private static final int m_myRobotRightEnd = 3;
+    private static final int m_myRobotRightMiddle = 2;
+    private static final int m_myRobotRightFront = 1;
+    private static Compressor compressor;
+    private DoubleSolenoid leftDouble = new DoubleSolenoid(0, 1);
+    private DoubleSolenoid rightDouble = new DoubleSolenoid(/* The PCM CAN ID */ 0, 2, 3); 
+}
