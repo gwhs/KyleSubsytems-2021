236156040c38bb5a98dcd1cff11f6f6d7c28f01b
Chris <chvoong@s.sfusd.edu>
2021-11-08 19:36:39 -0800

    changed subsystem and command names

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class DrivetrainSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public DrivetrainSubsystem() {}
 
   @Override
   public void periodic() {
