dd4d1925c670ecb17ee8d934c91574c1722eb2d6
Chris <chrsv00ng6@gmail.com>
2021-11-27 12:29:07 -0800

    added vision.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+ 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
@@ -28,6 +29,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    
     
   }
 
